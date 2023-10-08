74757524717de0863dadf736e01d006111fee454
murney <rmurney@gmail.com>
2023-10-08 18:31:42 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQrcode } from '@fortawesome/free-solid-svg-icons';
+import QRCode from 'react-qr-code';
 
 function App() {
+  const [qrValue, setQRValue] = useState('');
+
+  const changeQR = (e: any) => {
+    setQRValue(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="topNav">
+        <FontAwesomeIcon icon={faQrcode} size="lg" /> 
+        <span>QR Generator</span>
+      </div>
+      <input type="text" id="linkInput" onChange={changeQR}/>
+      <div className="qrContainer">
+        <QRCode
+          size={500}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          value={qrValue}
+          viewBox={`0 0 500 500`}
+        />
+      </div>
     </div>
   );
 }
